e6c5a2812ae5d28cb9cabbe8a59c81dc50b7d0e5
bighaeil@gmail.com <bighaeil@gmail.com>
2025-08-10 14:52:21 +0900

    fix NestJS Stable Server

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TraceIdMiddleware } from './common/middleware/trace-id.middleware';
+import { HttpLoggerMiddleware } from './common/middleware/http-logger.middleware';
+import { OrderModule } from './order/order.module';
+import { PaymentModule } from './payment/payment.module';
+import { APP_FILTER } from '@nestjs/core';
+import { GlobalExceptionFilter } from './common/filters/global-exception.filter';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [OrderModule, PaymentModule],
+    controllers: [AppController],
+    providers: [
+        AppService,
+        { provide: APP_FILTER, useClass: GlobalExceptionFilter },
+    ],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+    configure(consumer: MiddlewareConsumer) {
+        consumer.apply(TraceIdMiddleware, HttpLoggerMiddleware).forRoutes('*');
+    }
+}
